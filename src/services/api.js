aa65ecb49cb54a6d9df42c45a5cad8dbc0b91c9b
Vinícius Okaeda <61584826+ViniciusOkaeda@users.noreply.github.com>
2024-12-17 21:02:55 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://hub.youcast.tv.br/",
+  baseURL: "",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
